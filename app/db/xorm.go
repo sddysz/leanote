eec040a9d99b8c41758612f71b926b940c346314
sddysz@126.com <sddysz@126.com>
2017-05-19 17:36:15 +0800

    1

---
+++ app/db/xorm.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"github.com/revel/revel"
+)
+
+var (
+	Engine        *xorm.Engine
+	CachePageSize int // 允许缓存前几页数据
+)
+
+// init 初始化数据库引擎
+func Init() {
+	c := revel.Config
+	driver, _ := c.String("db.driver")
+	dbname, _ := c.String("db.dbname")
+	user, _ := c.String("db.username")
+	password, _ := c.String("db.password")
+	host, _ := c.String("db.host")
+	params := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true", user, password, host, dbname)
+	Engine, err := xorm.NewEngine(driver, params)
+	// defer Engine.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	// Engine.ShowSQL = revel.DevMode
+
+	err = Engine.Sync()
+
+	if err != nil {
+		panic(err)
+	}
+
+}
